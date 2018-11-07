8686de2a90da82eda2a974d0b185a1d9fb511171
Eminjan <eminjan123@aliyun.com>
2018-11-07 11:13:24 +0800

    Update settings.py

--- MxShop/settings.py
+++ MxShop/settings.py
@@ -100,7 +100,7 @@ DATABASES = {
         'NAME': 'vueshop',
         'USER': 'root',
         'PASSWORD': '123456',
-        'HOST': '132.232.109.153',
+        'HOST': '127.0.0.1',
         "OPTIONS": {"init_command": "SET default_storage_engine=INNODB;"}
     }
 }
